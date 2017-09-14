a7c6460aa8b6bdcd9c30b00d2a58b3e56f4ab0df
shimeng <meng.shi@aispeech.com>
2017-09-14 16:34:22 +0800

    修改调用

--- spider/html_parser.py
+++ spider/html_parser.py
@@ -7,7 +7,7 @@ import json
 import lxml.html as H
 from log_format import logger
 import inspect
-
+from tools import format_error_msg
 
 
 class HtmlParser(object):
@@ -16,57 +16,42 @@ class HtmlParser(object):
     在这里只提供接口
 
     """
+
     def __init__(self, ):
         self.logger = logger
 
     # xpath
-    def get_data_by_xpath(self, html_page_source,urls_xpath):
+    def get_data_by_xpath(self, html_page_source, urls_xpath):
         try:
             self.doc = H.document_fromstring(html_page_source)
         except Exception, e:
-            msg = 'Error msg: %s in [get_data_by_xpath]' %e
-            self.logger.error(self.format_error_msg(inspect.stack()[1][1], inspect.stack()[1][3], msg))
+            msg = 'Error msg: %s in [get_data_by_xpath]' % e
+            self.logger.error(format_error_msg(inspect.stack()[1][1], inspect.stack()[1][3], msg))
         else:
             data = self.doc.xpath(urls_xpath)
             return data
 
     # re
-    def get_data_by_re(self, html_page_source,pattern, flags=re.DOTALL):
+    def get_data_by_re(self, html_page_source, pattern, flags=re.DOTALL):
         try:
             data = re.findall(pattern, html_page_source, flags=flags)
         except Exception, e:
-            msg = 'Error msg: %s in [get_data_by_re]' %e
-            self.logger.error(self.format_error_msg(inspect.stack()[1][1], inspect.stack()[1][3], msg))
+            msg = 'Error msg: %s in [get_data_by_re]' % e
+            self.logger.error(format_error_msg(inspect.stack()[1][1], inspect.stack()[1][3], msg))
 
         else:
             return data
 
     # json
-    def get_data_by_json(self,html_page_source):
+    def get_data_by_json(self, html_page_source):
         try:
             data = json.loads(html_page_source)
         except Exception, e:
-            msg = 'Error msg: %s in [get_data_by_json]' %e
-            self.logger.error(self.format_error_msg(inspect.stack()[1][1], inspect.stack()[1][3], msg))
+            msg = 'Error msg: %s in [get_data_by_json]' % e
+            self.logger.error(format_error_msg(inspect.stack()[1][1], inspect.stack()[1][3], msg))
 
         else:
             return data
 
-    def format_error_msg(self, file_name, func_name, error_msg):
-        error_info = '''
-        detail_error_info
-        ##################
-        file_name: {},
-        func_name: {},
-        error_msg: {}
-        ##################
-        '''.format(file_name, func_name, error_msg)
-        return error_info
 
 parser = HtmlParser()
-
-if __name__ == '__main__':
-    content = json.dumps({'a':1})
-    html_parser = HtmlParser()
-    data = html_parser.get_data_by_json(content)
-    print data
