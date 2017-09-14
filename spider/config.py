113f9ad2e9dfa6de414fb68d650c80b9ed401e27
shimeng <meng.shi@aispeech.com>
2017-09-14 16:34:12 +0800

    添加说明

--- spider/config.py
+++ spider/config.py
@@ -17,21 +17,21 @@ retry_times = 10
 time_out = 5
 # 当use_proxy为True时，必须在请求的args中或者在配置文件中定义ip, eg: ip="120.52.72.58:80", 否则程序将报错
 use_proxy = False
-ip=None
+ip = None
+
+# 移动端设置为 ua_type = 'mobile'
 ua_type = 'pc'
 
 # 队列顺序
-FIFO=0
+FIFO = 0
 # 默认提供的浏览器头包括user_agent  host, 若需要更丰富的header,可自行定定义新的header,并赋值给diy_header
 header = {
-    'User-Agent':'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
-    'Referer':'https://y.qq.com/n/yqq/song/003OUlho2HcRHC.html',
-
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.90 Safari/537.36',
+    'Referer': 'https://y.qq.com/n/yqq/song/003OUlho2HcRHC.html',
 
 }
 
 diy_header = None
-
 
 # 定义状态码,不在其中的均视为请求错误或异常
 status_code = [200, 304, 404]
