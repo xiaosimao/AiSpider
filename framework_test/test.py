2ea6a2e6cea409b448e9cb13592884bb8b651a20
shimeng <meng.shi@aispeech.com>
2017-09-20 11:52:08 +0800

    将队列join放入主程序

--- framework_test/test.py
+++ framework_test/test.py
@@ -2,8 +2,8 @@
 # -*- coding: utf-8 -*-
 # Created by shimeng on 17-9-12
 # 导入模块
-
 import sys
+
 # 这里写你自己的地址
 sys.path.append('/home/shimeng/code/spider_framework_github_responsity')
 
@@ -11,9 +11,9 @@ from spider.tools import format_put_data
 from spider.data_save import pipeline
 from spider.html_parser import parser
 from spider.page_downloader import aispider
-from spider.threads import start, work_queue
+from spider.threads import start, work_queue, save_queue
 from spider.log_format import logger
-
+import threading
 
 root_url_format = 'https://c.y.qq.com/v8/fcg-bin/fcg_v8_singer_album.fcg?g_tk=676629472&loginUin=549411552&hostUin=0&format=jsonp&inCharset=utf8&outCharset=utf-8&notice=0&platform=yqq&needNewCode=0&singermid={singermid}&order=time&begin={begin}&num={num}&exstatus=1'
 
@@ -30,8 +30,9 @@ class SpiderMain(object):
         for singer_mid in singer_mids:
             url = root_url_format.format(singermid=singer_mid, begin=begin, num=num)
             # 调用format_put_data 构造放入队列中的数据 
-            put_data = format_put_data(args={"url": url}, work_func=self.downloader.request, follow_func=self.get_total_num)
-            # 放入队列                           
+            put_data = format_put_data(args={"url": url}, work_func=self.downloader.request,
+                                       follow_func=self.get_total_num)
+            # 放入队列
             work_queue.put(put_data)
 
     # 这个函数就是上一步中定义的follow_func
@@ -40,24 +41,26 @@ class SpiderMain(object):
         html_content = response.get('content')
         # 调用解析方法
         datas = parser.get_data_by_json(html_content)
-        total_num = datas.get('data').get('total','')
+        total_num = datas.get('data').get('total', '')
 
         if total_num:
             singer_mid = datas.get('data').get('singer_mid')
             url = root_url_format.format(singermid=singer_mid, begin=0, num=total_num)
             # 再次调用format_put_data方法构造将要放入工作队列中的数据
-            put_data = format_put_data(args={"url": url}, work_func=self.downloader.request,need_save=True, save_func=self.save)
+            put_data = format_put_data(args={"url": url}, work_func=self.downloader.request, need_save=True,
+                                       save_func=self.save)
             work_queue.put(put_data)
 
     # 上一步中定义的保存函数
     def save(self, response):
-        #ｍｏｎｇｏｄｂ集合名称
+        # ｍｏｎｇｏｄｂ集合名称
         collection_name = 'qq_music_album_base_info'
         html_content = response.get('content')
         datas = parser.get_data_by_json(html_content)
         data = datas.get('data').get('list')
         if data:
             for da in data:
+                print da
                 insert_data = da
                 insert_data['singer_id'] = datas.get('data').get('singer_id')
                 insert_data['singer_mid'] = datas.get('data').get('singer_mid')
@@ -65,17 +68,21 @@ class SpiderMain(object):
                 insert_data['total'] = datas.get('data').get('total')
                 insert_data['_id'] = da.get('albumID')
 
-                #　保存数入库　
-                self.pipeline.process_item(insert_data, collection_name)
+                # 　保存数入库　
+                # self.pipeline.process_item(insert_data, collection_name)
+
 
 def go(singer_mids):
     start()
     obj_spider = SpiderMain()
     obj_spider.craw(singer_mids)
-    # 这里必须写这个
-    work_queue.join()
+
 
 if __name__ == '__main__':
-
     singer_mids = ['0025NhlN2yWrP4']
     go(singer_mids)
+
+    # blocking
+    work_queue.join()
+    save_queue.join()
+    print 'done'
